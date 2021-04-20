0ca4212a63b8da1db02bba37fee3c57363d64607
srzabala92 <sezabalam@alumnos.unex.es>
2021-04-20 19:30:03 +0200

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Probando Immersion Day"}
+		res := &response{Message: "Pruebas Immersion Day"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
